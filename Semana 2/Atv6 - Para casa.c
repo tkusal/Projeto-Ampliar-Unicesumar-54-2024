b0c8761f93c0cc470ad57191f1c29cd10a48cf7c
Iasmyn Lopes <iasminlopes71@gmail.com>
2024-10-08 17:28:26 -0300

    Update Atv6 - Para casa.c

--- Semana 2/Atv6 - Para casa.c
+++ Semana 2/Atv6 - Para casa.c
@@ -3,3 +3,31 @@
     O programa deverá calcular um bônus de 5% sobre o salário se o tempo de trabalho for superior a 5 anos.
 */
 
+#include <stdio.h>
+
+int main()
+{
+
+    int temptrab;
+    float salarioat, salcbonus, vbonus;
+
+    printf("Digite o salario ganho: ");
+    scanf("%f", &salarioat);
+
+    printf("Digite o tempo de trabalho em anos: ");
+    scanf("%d", &temptrab);
+
+    if (temptrab > 5) 
+    {
+        vbonus = salarioat * 0.05;
+        salcbonus = salarioat + vbonus;
+
+        printf("Seu salario com bonus e de: %.2f", salcbonus);
+    }
+    else 
+    {
+        printf("Voce nao recebe bonus");
+    }
+
+        return 0;
+}
