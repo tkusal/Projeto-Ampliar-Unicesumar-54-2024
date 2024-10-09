288ee569514e6c087e3249a6ae71cdb623649dd9
tkusal <thiago.kusal@outlook.com>
2024-10-08 22:27:28 -0300

    Aula 08/10/2024

--- Semana 2/Atv6 - Para casa.c
+++ Semana 2/Atv6 - Para casa.c
@@ -1,5 +1,38 @@
 /*
     Escreva um programa em C que peça ao usuário o salário e o tempo de trabalho em anos.
     O programa deverá calcular um bônus de 5% sobre o salário se o tempo de trabalho for superior a 5 anos.
+
+    salario * 0,05
 */
 
+#include <stdio.h>
+
+int main() {
+
+    float salario, bonus;
+    int anosTrabalhados;
+
+    printf("Informe o salário: ");
+    scanf("%f", &salario);
+
+    printf("Informe quantos anos de trabalho você tem: ");
+    scanf("%d", &anosTrabalhados);
+
+    if(anosTrabalhados > 5) {
+        //printf("Seu salário aumentou R$%f \nNovo salário: R$%f", salario*0.05, salario*0.05+salario);
+
+        /*
+        salario += salario * 0.05
+        Que é o mesmo que salario = salario * 0.05 + salario
+        */
+
+       bonus = salario * 0.05;
+       salario = salario + bonus;  //Que é o mesmo que salario += bonus;
+
+       printf("Seu salário aumentou R$%.2f \nNovo salário: R$%.2f", bonus, salario);
+    } else {
+        printf("Você não teve bonus salarial pois ainda não possui mais de 5 anos de trabalho.");
+    }
+
+    return 0;
+}
---
+++ Semana 3/Atv1.c
@@ -0,0 +1,38 @@
+/*
+    Escreva um programa em C, que receba a idade de 3 pessoas e informe
+    qual é a pessoa mais velha.
+*/
+
+#include <stdio.h> //STabdarD Input Output . Hearder
+
+int main()
+{
+
+    int pessoa1, pessoa2, pessoa3;
+
+    printf("Informe a idade da pessoa 1: ");
+    scanf("%d", &pessoa1);
+    printf("Informe a idade da pessoa 2: ");
+    scanf("%d", &pessoa2);
+    printf("Informe a idade da pessoa 3: ");
+    scanf("%d", &pessoa3);
+
+    if (pessoa1 > pessoa2 && pessoa1 > pessoa3)
+    {
+        printf("A Pessoa 1 é mais velha, com %d anos.", pessoa1);
+    }
+    else if (pessoa2 > pessoa1 && pessoa2 > pessoa3)
+    {
+        printf("A Pessoa 2 é mais velha, com %d anos.", pessoa2);
+    }
+    else if (pessoa3 > pessoa1 && pessoa3 > pessoa2)
+    {
+        printf("A Pessoa 2 é mais velha, com %d anos.", pessoa3);
+    }
+    else
+    {
+        printf("Não há uma idade maior.");
+    }
+
+    return 0;
+}
