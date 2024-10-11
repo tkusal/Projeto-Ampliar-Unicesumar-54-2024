45280a9d7de0ae36f1f76405fc76634fe9d211e8
tkusal <thiago.kusal@outlook.com>
2024-10-11 01:33:52 -0300

    ajuste

--- Semana 2/Atv6 - Para casa - Corregido.c
+++ Semana 2/Atv6 - Para casa - Corregido.c
@@ -2,7 +2,7 @@
     Escreva um programa em C que peça ao usuário o salário e o tempo de trabalho em anos.
     O programa deverá calcular um bônus de 5% sobre o salário se o tempo de trabalho for superior a 5 anos.
 
-    salario * 0,05
+    "salario * 0,05"
 */
 
 #include <stdio.h>
--- Semana 3/Atv6 - Para casa.c
+++
@@ -1,5 +0,0 @@
-/*
-    Escreva um programa em C que peça ao usuário o salário e o tempo de trabalho em anos.
-    O programa deverá calcular um bônus de 5% sobre o salário se o tempo de trabalho for superior a 5 anos.
-*/
-
