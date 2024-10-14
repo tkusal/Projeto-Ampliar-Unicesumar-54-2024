6220fb26a0b84d3215a843c4386fa03879d157a2
Iasmyn Lopes <iasminlopes71@gmail.com>
2024-10-14 17:50:24 -0300

    Atv9 resolvida - IasmynLopes

--- Semana 3/Atv9 - Para Casa.c
+++ Semana 3/Atv9 - Para Casa.c
@@ -3,3 +3,23 @@
     Exemplo: Leu 5 e 3, vai calcular 5³.
 */
 
+#include <stdio.h>
+
+int main() {
+    int numA, numB; 
+    float result = 1; 
+
+    printf("Digite um numero: ");
+    scanf("%d", &numA);
+
+    printf("Digite outro numero: ");
+    scanf("%d", &numB);
+    
+    for (int i = 0; i < numB; i++)
+    {
+       result = result * numA;
+    }
+        printf("O resultado da potencia de %d ^ %d = %.2f", numA, numB, result);
+
+    return 0;
+}
