e1d8d158bf1f8f79738735d75a375cf3a0652367
Iasmyn Lopes <iasminlopes71@gmail.com>
2024-10-14 17:53:16 -0300

    Atv11 resolvida - IasmynLopes

--- Semana 3/Atv11 - Para Casa.c
+++ Semana 3/Atv11 - Para Casa.c
@@ -4,3 +4,24 @@
     Exemplo: 1234 vira 4321.
 */
 
+#include <stdio.h>
+
+int main() {
+    int numi;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numi);
+
+    while (numi == 0)
+    {
+        printf("Digite um numero diferente de 0: ");
+        scanf("%d", &numi);
+    }
+
+    for (int i = numi; i > 0; i--)
+    {
+       printf("%d", i);
+    }
+    
+    return 0;
+}
