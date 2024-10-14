fc3d0a7ec1d2bd3033c592a28c227145dba12e0c
Iasmyn Lopes <iasminlopes71@gmail.com>
2024-10-14 17:52:05 -0300

    Atv10 resolvida - IasmynLopes

--- Semana 3/Atv10 - Para Casa.c
+++ Semana 3/Atv10 - Para Casa.c
@@ -7,3 +7,52 @@
 
     O programa só poderá ser finalizado quando o usuário escolher a opção 3
 */
+
+#include <stdio.h>
+
+int main() {
+    int opcao, num1, num2, result; 
+
+        do
+        {
+        printf("1 - Somar 2 numeros\n");
+        printf("2 - Multiplicar 2 numeros\n");
+        printf("3 - Encerrar\n");
+    
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+    if (opcao < 3)
+    {
+        switch (opcao)
+        {
+            case 1:
+            printf("Digite um numero para somar: ");
+            scanf("%d", &num1);
+
+            printf("Digite outro numero: ");
+            scanf("%d", &num2);
+
+            result = num1 + num2;
+            printf("A soma dos valores de %d + %d = %d\n", num1, num2, result);
+            break;
+
+            case 2:
+            printf("Digite um numero para multiplicar: ");
+            scanf("%d", &num1);
+
+            printf("Digite outro numero: ");
+            scanf("%d", &num2);
+
+            result = num1 * num2;
+            printf("A multiplicacao dos valores de %d * %d = %d\n", num1, num2, result);
+            break;
+        } 
+    } 
+        } while (opcao < 3);
+
+    if (opcao == 3)
+    {
+        return 0;
+    }
+} 
